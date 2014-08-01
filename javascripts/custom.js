e2adcd4447d779390766e1968f4dddeb962451f7
Shan Du <shan.du.sdu@gmail.com>
2014-08-01 01:34:58 -0400

    added skills tag cloud

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,28 +1,56 @@
+var pageContext = (function(){
+	
+	var initElements = function() {
+		$('.skill-cloud a').tagcloud();
+	};
+	
+	var bindPageEvents = function() {
+		$("header").find(".view-resume").click(function(e){
+			e.preventDefault();
+			
+			var _this = $(this);
+			var html = _this.html();
+			
+			if (html.indexOf("View") >= 0) {
+				$("#intro").fadeOut("fast", function(){
+					$("#profile").fadeIn("fast");
+				});
+				$(_this.html(html.replace("View","Hide")));
+			}
+			else {
+				$("#profile").fadeOut("fast", function(){
+					$("#intro").fadeIn("fast");
+				});
+				$(_this.html(html.replace("Hide","View")));
+			}
+		});
+	};
+	
+	function _config() {
+		$.fn.tagcloud.defaults = {
+			size: {start: 0.8, end: 1.6, unit: 'em'},
+			color: {start: '#888', end: '#000'}
+		};
+	}
+	
+	function _init() {
+		initElements();
+		bindPageEvents();
+	}
+	
+	return {
+		config : _config,
+		init : _init
+	};
+})(jQuery);
+
+
 (function($) {
 
+	pageContext.config();
 
-$(document).ready(function(){
-
-	$("header").find(".view-resume").click(function(e){
-		e.preventDefault();
-		
-		var _this = $(this);
-		var html = _this.html();
-		
-		if (html.indexOf("View") >= 0) {
-			$("#intro").fadeOut("fast", function(){
-				$("#profile").fadeIn("fast");
-			});
-			$(_this.html(html.replace("View","Hide")));
-		}
-		else {
-			$("#profile").fadeOut("fast", function(){
-				$("#intro").fadeIn("fast");
-			});
-			$(_this.html(html.replace("Hide","View")));
-		}
+	$(document).ready(function(){
+		pageContext.init();
 	});
-
-});
 
 })(jQuery)
