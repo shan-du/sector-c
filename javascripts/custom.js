a65d25f0ebf87217146bf0993a3cd7ba49055f8e
Shan Du <shan.du.sdu@gmail.com>
2014-08-01 00:15:32 -0400

    content update, added toggle

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,8 +1,27 @@
 (function($) {
 
+
 $(document).ready(function(){
 
-
+	$("header").find(".view-resume").click(function(e){
+		e.preventDefault();
+		
+		var _this = $(this);
+		var html = _this.html();
+		
+		if (html.indexOf("View") >= 0) {
+			$("#intro").fadeOut("fast", function(){
+				$("#profile").fadeIn("fast");
+			});
+			$(_this.html(html.replace("View","Hide")));
+		}
+		else {
+			$("#profile").fadeOut("fast", function(){
+				$("#intro").fadeIn("fast");
+			});
+			$(_this.html(html.replace("Hide","View")));
+		}
+	});
 
 });
 
