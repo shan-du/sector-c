a646a0c1596c57fc003e75b1f4eb18651945a24d
Shan Du <shan.du.sdu@gmail.com>
2016-05-14 01:27:59 -0400

    added profile page & updated navigation menu

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -5,24 +5,20 @@ var pageContext = (function(){
 	};
 	
 	var bindPageEvents = function() {
+		$("header").find(".about-shan").click(function(e){
+			e.preventDefault();
+
+			$("#profile").fadeOut("fast", function(){
+				$("#intro").fadeIn("fast");
+			});
+		});
+		
 		$("header").find(".view-resume").click(function(e){
 			e.preventDefault();
-			
-			var _this = $(this);
-			var html = _this.html();
-			
-			if (html.indexOf("View") >= 0) {
-				$("#intro").fadeOut("fast", function(){
-					$("#profile").fadeIn("fast");
-				});
-				$(_this.html(html.replace("View","Hide")));
-			}
-			else {
-				$("#profile").fadeOut("fast", function(){
-					$("#intro").fadeIn("fast");
-				});
-				$(_this.html(html.replace("Hide","View")));
-			}
+
+			$("#intro").fadeOut("fast", function(){
+				$("#profile").fadeIn("fast");
+			});
 		});
 	};
 	
